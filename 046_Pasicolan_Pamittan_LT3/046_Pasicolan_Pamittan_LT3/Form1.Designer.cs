ea5d0aa98dec662d7dec0fa3ad84908f07082ae4
paui <163615470+httppaui@users.noreply.github.com>
2024-09-13 15:07:30 +0800

    Add files via upload

---
+++ 046_Pasicolan_Pamittan_LT3/046_Pasicolan_Pamittan_LT3/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace _046_Pasicolan_Pamittan_LT3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.excelFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboDept = new System.Windows.Forms.ComboBox();
+            this.btnAddtoList = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.grdData = new System.Windows.Forms.DataGridView();
+            this.btnResetForm = new System.Windows.Forms.Button();
+            this.btnSaveRec = new System.Windows.Forms.Button();
+            this.openExcel = new System.Windows.Forms.OpenFileDialog();
+            this.openText = new System.Windows.Forms.OpenFileDialog();
+            this.saveText = new System.Windows.Forms.SaveFileDialog();
+            this.btnCheckRec = new System.Windows.Forms.Button();
+            this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip.Size = new System.Drawing.Size(838, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.excelFileToolStripMenuItem,
+            this.textFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.fileToolStripMenuItem.Text = "Data Source";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // excelFileToolStripMenuItem
+            // 
+            this.excelFileToolStripMenuItem.Name = "excelFileToolStripMenuItem";
+            this.excelFileToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.excelFileToolStripMenuItem.Text = "Excel File";
+            this.excelFileToolStripMenuItem.Click += new System.EventHandler(this.excelFileToolStripMenuItem_Click);
+            // 
+            // textFileToolStripMenuItem
+            // 
+            this.textFileToolStripMenuItem.Name = "textFileToolStripMenuItem";
+            this.textFileToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.textFileToolStripMenuItem.Text = "Text File";
+            this.textFileToolStripMenuItem.Click += new System.EventHandler(this.textFileToolStripMenuItem_Click);
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(32, 73);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(263, 20);
+            this.txtFName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Last Name";
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(32, 139);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(263, 20);
+            this.txtLName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Department";
+            // 
+            // cboDept
+            // 
+            this.cboDept.FormattingEnabled = true;
+            this.cboDept.Location = new System.Drawing.Point(32, 208);
+            this.cboDept.Name = "cboDept";
+            this.cboDept.Size = new System.Drawing.Size(263, 21);
+            this.cboDept.TabIndex = 6;
+            // 
+            // btnAddtoList
+            // 
+            this.btnAddtoList.Location = new System.Drawing.Point(68, 271);
+            this.btnAddtoList.Name = "btnAddtoList";
+            this.btnAddtoList.Size = new System.Drawing.Size(75, 23);
+            this.btnAddtoList.TabIndex = 7;
+            this.btnAddtoList.Text = "Add to List";
+            this.btnAddtoList.UseVisualStyleBackColor = true;
+            this.btnAddtoList.Click += new System.EventHandler(this.btnAddtoList_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(171, 271);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // grdData
+            // 
+            this.grdData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grdData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdData.Location = new System.Drawing.Point(323, 60);
+            this.grdData.MultiSelect = false;
+            this.grdData.Name = "grdData";
+            this.grdData.ReadOnly = true;
+            this.grdData.RowHeadersVisible = false;
+            this.grdData.RowHeadersWidth = 51;
+            this.grdData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdData.Size = new System.Drawing.Size(486, 264);
+            this.grdData.TabIndex = 9;
+            // 
+            // btnResetForm
+            // 
+            this.btnResetForm.Location = new System.Drawing.Point(731, 344);
+            this.btnResetForm.Name = "btnResetForm";
+            this.btnResetForm.Size = new System.Drawing.Size(75, 23);
+            this.btnResetForm.TabIndex = 11;
+            this.btnResetForm.Text = "Reset Form";
+            this.btnResetForm.UseVisualStyleBackColor = true;
+            this.btnResetForm.Click += new System.EventHandler(this.btnResetForm_Click);
+            // 
+            // btnSaveRec
+            // 
+            this.btnSaveRec.Location = new System.Drawing.Point(602, 344);
+            this.btnSaveRec.Name = "btnSaveRec";
+            this.btnSaveRec.Size = new System.Drawing.Size(101, 23);
+            this.btnSaveRec.TabIndex = 10;
+            this.btnSaveRec.Text = "Save Records";
+            this.btnSaveRec.UseVisualStyleBackColor = true;
+            this.btnSaveRec.Click += new System.EventHandler(this.btnSaveRec_Click);
+            // 
+            // btnCheckRec
+            // 
+            this.btnCheckRec.Location = new System.Drawing.Point(602, 382);
+            this.btnCheckRec.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCheckRec.Name = "btnCheckRec";
+            this.btnCheckRec.Size = new System.Drawing.Size(101, 19);
+            this.btnCheckRec.TabIndex = 12;
+            this.btnCheckRec.Text = "Check Record";
+            this.btnCheckRec.UseVisualStyleBackColor = true;
+            this.btnCheckRec.Click += new System.EventHandler(this.btnCheckRec_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 432);
+            this.Controls.Add(this.btnCheckRec);
+            this.Controls.Add(this.btnResetForm);
+            this.Controls.Add(this.btnSaveRec);
+            this.Controls.Add(this.grdData);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAddtoList);
+            this.Controls.Add(this.cboDept);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem excelFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textFileToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboDept;
+        private System.Windows.Forms.Button btnAddtoList;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView grdData;
+        private System.Windows.Forms.Button btnResetForm;
+        private System.Windows.Forms.Button btnSaveRec;
+        private System.Windows.Forms.OpenFileDialog openExcel;
+        private System.Windows.Forms.OpenFileDialog openText;
+        private System.Windows.Forms.SaveFileDialog saveText;
+        private System.Windows.Forms.Button btnCheckRec;
+    }
+}
+
---
+++ 046_Pasicolan_Pamittan_LT3/046_Pasicolan_Pamittan_LT3/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+namespace _046_Pasicolan_Pamittan_LT3
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeDataTable()
+        {
+            if (dt.Columns.Count == 0)
+            {
+                dt.Columns.Add("First Name");
+                dt.Columns.Add("Last Name");
+                dt.Columns.Add("Department");
+                grdData.DataSource = dt;
+            }
+        }
+
+        private void excelFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openExcel.Title = "Open Excel";
+            openExcel.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openExcel.Filter = "All files (*.*)|*.*|Excel File (*.xlsx)|*.xlsx";
+            openExcel.FilterIndex = 2;
+            openExcel.ShowDialog();
+
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source =" + openExcel.FileName + "; Extended Properties='Excel 12.0 Xml;HDR=Yes'");
+            OleDbCommand cmd = new OleDbCommand("SELECT * FROM [Sheet1$]", conn);
+            OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            cboDept.DataSource = dt;
+            cboDept.DisplayMember = "dept";
+        }
+
+        private void textFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openText.Title = "Open Text";
+            openText.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openText.Filter = "All files (*.*)|*.*|Text File (*.txt)|*.txt";
+            openText.FilterIndex = 2;
+            openText.ShowDialog();
+
+            if (!String.IsNullOrWhiteSpace(openText.FileName))
+            {
+                string[] statement = File.ReadAllLines(openText.FileName);
+                for (int i = 0; i < statement.Length; i++)
+                {
+                    cboDept.Items.Add(statement[i]);
+                }
+            }
+        }
+
+        private void btnAddtoList_Click(object sender, EventArgs e)
+        {
+            InitializeDataTable();
+
+            if (!String.IsNullOrEmpty(txtFName.Text) && !String.IsNullOrEmpty(txtLName.Text) && !String.IsNullOrEmpty(cboDept.SelectedIndex.ToString()))
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    DataRow row = dt.NewRow();
+                    row["First Name"] = txtFName.Text;
+                    row["Last Name"] = txtLName.Text;
+                    row["Department"] = cboDept.Text;
+                    dt.Rows.Add(row);
+                }
+
+                txtFName.Text = string.Empty;
+                txtLName.Text = string.Empty;
+                cboDept.SelectedIndex = -1;
+            }
+        }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+
+            }
+
+        private void btnResetForm_Click(object sender, EventArgs e)
+        {
+            txtFName.Clear();
+            txtLName.Clear();
+            cboDept.SelectedIndex = -1;
+            grdData.DataSource = null;
+        }
+
+        private void btnSaveRec_Click(object sender, EventArgs e)
+        {
+            saveText.Title = "Save as file";
+            saveText.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            saveText.DefaultExt = "txt";
+            saveText.Filter = "All files (*.*)|*.*|Text File (*.txt)|*.txt";
+            saveText.FilterIndex = 2;
+            if (saveText.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = File.CreateText(saveText.FileName))
+                {
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        writer.WriteLine($"{row["First Name"]}");
+                        writer.WriteLine($"{row["Last Name"]}");
+                        writer.WriteLine($"{row["Department"]}");
+                    }
+                }
+                MessageBox.Show("Text file saved successfully!", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCheckRec_Click(object sender, EventArgs e)
+        {
+            openText.Title = "Open Text";
+            openText.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openText.Filter = "All files (*.*)|*.*|Text File (*.txt)|*.txt";
+            openText.FilterIndex = 2;
+            openText.ShowDialog();
+
+            if (!String.IsNullOrWhiteSpace(openText.FileName))
+            {
+                string[] information = File.ReadAllLines(openText.FileName);
+                DataTable dt = new DataTable();
+                dt.Columns.Add("First Name");
+                dt.Columns.Add("Last Name");
+                dt.Columns.Add("Department");
+
+                for (int i = 0; i < information.Length; i += 3)
+                {
+                    if (i + 2 < information.Length)
+                    {
+                        DataRow row = dt.NewRow();
+                        row["First Name"] = information[i];
+                        row["Last Name"] = information[i + 1];
+                        row["Department"] = information[i + 2];
+                        dt.Rows.Add(row);
+                    }
+                }
+
+                grdData.DataSource = dt;
+            }
+        }
+    }   
+    }
+
+
+
